ceb280e0e8d9dc06e5349ef138be72cffa6ed08f
Maham web developer <125392502+Maham-web-developer@users.noreply.github.com>
2024-01-24 00:19:26 -0800

    Add files via upload

---
+++ Login.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+    <div className="container">
+      <form action="">
+       <input type="text" placeholder='name'  required/>
+       <input type="email" placeholder='email'  required/>
+       <input type="number" placeholder='phone' required/>
+       
+        
+
+      </form>
+    </div>
+    </div>
+  )
+}
+
+export default Login
